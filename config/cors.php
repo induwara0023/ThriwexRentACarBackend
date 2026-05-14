1cf83f40512bf3a844a551f4672f648660db6b82
induwara0023 <170860970+induwara0023@users.noreply.github.com>
2026-05-14 19:24:04 +0530

    Fix CORS policy for new domain

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://nsrentacarmanager.online', 'http://localhost:4200'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
